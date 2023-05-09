b8196eafaea14afd460c70ee76fcea4b197816b0
thomas <phanthomas.tai@gfk.com>
2023-05-09 10:45:23 +0700

    add changes

--- src/modules/cron-report/cron-report.service.ts
+++ src/modules/cron-report/cron-report.service.ts
@@ -48,6 +48,7 @@ export class CronReportService implements OnModuleInit {
     const { project_name, time_start, working_time, time_end, job, status, note } = cron.report
     const user = cron.report.user
     const browser = await puppeteer.launch({
+      headless: "new",
       ignoreHTTPSErrors: true,
       ignoreDefaultArgs: ['--enable-automation'],
       args: [
